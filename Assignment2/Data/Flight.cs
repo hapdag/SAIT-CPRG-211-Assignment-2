e78f4b86e0e647aea1141a00f52078de92e27be8
Si He <SiYuan.He@edu.sait.ca>
2024-07-19 21:36:22 -0600

    Add CSV files to project resources

---
+++ Assignment2/Data/Flight.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2.Data
+{
+    internal class Flight
+    {
+        private string _flightNumber;
+        private string _airline;
+        private string _departure;
+        private string _arrival;
+        private string _flightDay;
+        private string _flightTime;
+        private int _seats;
+        private double _ticketCost;
+        public string FlightNumber { get { return _flightNumber; } }
+        public string Airline { get { return _airline; } }
+        public string Departure { get { return _departure; } }
+        public string Arrival { get { return _arrival; } }
+        public string FlightDay { get { return _flightDay; } }
+        public string FlightTime { get { return _flightTime; } }
+        public int Seats {  get { return _seats; } }
+        public double TicketCost {  get { return _ticketCost; } }
+
+        public Flight( string flightNumber, string airline, string departure, string arrival, string flightDay, string flightTime, int seats, double ticketCost)
+        {
+            _flightNumber = flightNumber;
+            _airline = airline;
+            _departure = departure;
+            _arrival = arrival;
+            _flightDay = flightDay;
+            _flightTime = flightTime;
+            _seats = seats;
+            _ticketCost = ticketCost;
+        }
+    }
+}
